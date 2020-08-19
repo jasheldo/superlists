38b5eef9caf88342c70440f0dde851582ac4fa54
James Sheldon <jimsheldon@icloud.com>
2020-08-18 23:01:37 -0500

    Adjusted model so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class List(models.Model):
+    """docstring"""
+    pass
+
+
 class Item(models.Model):
     """docstring"""
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
