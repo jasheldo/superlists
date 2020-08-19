40e6156d6e89a304f5f88af334f43eb59a7b4293
James Sheldon <jimsheldon@icloud.com>
2020-08-18 20:04:28 -0500

    simplified views and cleaned up unit tests

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,6 @@ from lists.models import Item
 
 def home_page(request):
     """docstring"""
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
@@ -16,3 +12,9 @@ def view_list(request):
     """docstring"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """docstring"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
